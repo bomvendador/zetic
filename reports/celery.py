f6b857cf8abe8877e1c7f99808b9c6a5ef0bc3e4
Алексей <bomvendador@yandex.ru>
2022-12-17 18:20:03 +0300

    1

--- reports/celery.py
+++ reports/celery.py
@@ -21,3 +21,32 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+app.conf.beat_schedule = {
+    # Scheduler Name
+    'print-message-ten-seconds': {
+        # Task Name (Name Specified in Decorator)
+        'task': 'print_msg_main',
+        # Schedule
+        'schedule': 10.0,
+        # Function Arguments
+        'args': ("Hello",)
+    },
+    # Scheduler Name
+    'print-time-twenty-seconds': {
+        # Task Name (Name Specified in Decorator)
+        'task': 'print_time',
+        # Schedule
+        'schedule': 20.0,
+    },
+    # Scheduler Name
+    'calculate-forty-seconds': {
+        # Task Name (Name Specified in Decorator)
+        'task': 'get_calculation',
+        # Schedule
+        'schedule': 40.0,
+        # Function Arguments
+        'args': (10, 20)
+    },
+}
