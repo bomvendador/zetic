00b4b337acce1f4231245be00074bf6cfb62ecfb
Алексей <bomvendador@yandex.ru>
2022-12-17 17:41:44 +0300

    1

--- reports/settings.py
+++ reports/settings.py
@@ -104,6 +104,15 @@ DATABASES = {
 }
 
 
+# Celery Broker - Redis
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = "Europe/Moscow"
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
