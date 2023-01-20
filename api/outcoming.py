f61796b25ad9e8f6a8cba7efe9fcd1a5cfa7f666
Алексей <bomvendador@yandex.ru>
2023-01-20 23:42:44 +0300

    1

--- api/outcoming.py
+++ api/outcoming.py
@@ -112,7 +112,9 @@ def sync_add_employee(employee_id):
         "sex_id": employee.sex.public_code
     }
     print(data)
-    response = requests.post(settings.API_LINK + 'company/' + company_id + '/employee',
+    url = settings.API_LINK + 'company/' + company_id + '/employee'
+    print(url)
+    response = requests.post(url,
                             headers={'Authorization': 'Bearer ' + settings.API_BEARER}, data=data)
     # print(f'sync response - {response}')
     # return response
