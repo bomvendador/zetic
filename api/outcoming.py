014d3d05e95d8b5b34d63d2a8fb5485bced6d946
Алексей <bomvendador@yandex.ru>
2023-01-20 13:46:08 +0300

    1

--- api/outcoming.py
+++ api/outcoming.py
@@ -94,8 +94,8 @@ def sync_add_company(name, public_code):
         "public_code": public_code
     }
     response = requests.post(settings.API_LINK + 'company',
-                            headers={'Authorization': 'Bearer ' + settings.API_BEARER}, json=data)
-    print(response)
+                            headers={'Authorization': 'Bearer ' + settings.API_BEARER}, data=json.dumps(data))
+    print(f'sync response - {response}')
 
 
 
