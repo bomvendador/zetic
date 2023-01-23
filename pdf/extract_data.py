3cfe0290a9a97cb206319c1aa18909f95ef2b186
Алексей <bomvendador@yandex.ru>
2023-01-23 22:54:22 +0300

    1

--- pdf/extract_data.py
+++ pdf/extract_data.py
@@ -1,5 +1,6 @@
 from pdf.models import PointDescription
 from . import raw_to_t_point
+from django.utils import timezone
 
 
 def extract_section(request_json, section_code):
@@ -13,7 +14,7 @@ def extract_categories(json_section, category_code, lang, participant_info):
         if category['code'] == category_code:
             category_point = raw_to_t_point.get_t_point(category['points'], category_code, participant_info['sex'], int(participant_info['year']))
             # category_point = category['points']
-            print(f'{category_code} - {category["points"]} - {category_point}')
+            print(f'{timezone.localtime(timezone.now()).strftime("%d.%m.%Y %H:%M:%S")} - {category_code} - {category["points"]} - {category_point}')
             if category_point == 0:
                 return {'points': category_point, 'point_description': ''}
             else:
