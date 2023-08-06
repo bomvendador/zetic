3ef98413fc111834fc293a0ff9c7ec0245cdf2d4
Pavel Borskyシ <dancetrain@users.noreply.github.com>
2023-08-06 23:19:46 +0200

    GroupReport Refactoring

--- pdf/group_report.py
+++ pdf/group_report.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import List, Dict, Tuple
@@ -69,253 +70,284 @@ class GroupReportData:
     square_results: List[SquareResult]
 
 
-def _draw_square(
-    pdf,
-    x: int,
-    y: int,
-    width: int,
-    height: int,
-    color: Tuple[int, int, int],
-    label: str = None,
-):
-    pdf.set_draw_color(*color)
-    pdf.set_fill_color(*color)
-    pdf.rect(x, y, width, height, "FD")
-
-    pdf.set_draw_color(255, 255, 255)
-    pdf.set_fill_color(255, 255, 255)
-    # белый вертикальный
-    pdf.rect(
-        x=x + width / 2 - 0.3,
-        y=y,
-        w=0.6,
-        h=x + width * 2,
-        style="FD",
-    )
-    # белый горизонтальный
-    pdf.rect(
-        x=x,
-        y=y + width / 2 - 0.3,
-        w=width,
-        h=0.6,
-        style="FD",
-    )
-
-    # square label
-    pdf.set_label_font(10)
-    pdf.set_text_color(0, 0, 0)
-    # in the center
-    center_x = x + width / 2
-    center_y = y + height / 2
-    label_with = pdf.get_string_width(label)
-    rect_width = label_with + 10  # 5*2 padding
-    rect_height = 10
-    pdf.rect(
-        center_x - rect_width / 2,
-        center_y - rect_height / 2,
-        rect_width,
-        rect_height,
-        "FD",
-    )
-    pdf.set_xy(x=center_x - rect_width / 2, y=center_y - rect_height / 2)
-    pdf.cell(
-        w=rect_width,
-        h=rect_height,
-        align=Align.C,
-        txt=label,
-    )
-    pass
-
-
-def _draw_group_profile_squares(pdf: ZeticPDF):
-    start_x = pdf.get_x()
-    start_y = pdf.get_y()
-    width = (pdf.w - pdf.l_margin - pdf.r_margin) / 2
-
-    _draw_square(pdf, start_x, start_y, width, width, (255, 240, 193), "Интеграторы")
-    _draw_square(
-        pdf, start_x + width, start_y, width, width, (253, 219, 246), "Предприниматели"
-    )
-
-    _draw_square(
-        pdf,
-        start_x,
-        start_y + width,
-        width,
-        width,
-        (217, 245, 251),
-        "Администраторы",
-    )
-    # 4
-    _draw_square(
-        pdf,
-        start_x + width,
-        start_y + width,
-        width,
-        width,
-        (226, 239, 218),
-        "Производители",
-    )
-
-    pdf.set_text_font(8)
-    pdf.text(start_x + width / 2 - 13, start_y - 2.5, "Фокус на процесс")
-    pdf.text(start_x + width / 2 + width - 13.5, start_y - 2.5, "Фокус на результат")
-    with pdf.rotation(90, pdf.l_margin - pdf.font_size, start_y + width / 2 + 10):
-        pdf.text(0, start_y + width / 2 + 10, "Неструктурированный подход")
-    with pdf.rotation(270, start_x + width * 2 + 3, start_y + width / 2 - 18):
-        pdf.text(
-            start_x + width * 2 + 3,
-            start_y + width / 2 - 18,
-            "Концептуальные решения",
-        )
-    with pdf.rotation(
-        90, pdf.l_margin - pdf.font_size, start_y + width / 2 + 7 + width
+class ZeticGroupPDF(ZeticPDF):
+    def draw_square(
+        self,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        color: Tuple[int, int, int],
+        label: str = None,
+        label_desc: str = None,
     ):
-        pdf.text(
-            0,
-            start_y + width / 2 + 7 + width,
-            "Структурированный подход",
+        self.set_draw_color(*color)
+        self.set_fill_color(*color)
+        self.rect(x, y, width, height, "FD")
+
+        self.set_draw_color(255, 255, 255)
+        self.set_fill_color(255, 255, 255)
+        # белый вертикальный
+        self.rect(
+            x=x + width / 2 - 0.3,
+            y=y,
+            w=0.6,
+            h=x + width * 2,
+            style="FD",
         )
-    with pdf.rotation(
-        270,
-        start_x + width * 2 + 3,
-        start_y + width / 2 - 15 + width,
-    ):
-        pdf.text(
+        # белый горизонтальный
+        self.rect(
+            x=x,
+            y=y + width / 2 - 0.3,
+            w=width,
+            h=0.6,
+            style="FD",
+        )
+
+        # square label
+        self.set_text_color(0, 0, 0)
+        # in the center
+        center_x = x + width / 2
+        center_y = y + height / 2
+        label_with = 28
+        rect_width = label_with + 10  # 5*2 padding
+        rect_height = 12
+        self.rect(
+            center_x - rect_width / 2,
+            center_y - rect_height / 2,
+            rect_width,
+            rect_height,
+            "FD",
+        )
+        self.set_xy(x=center_x - rect_width / 2, y=center_y - rect_height / 2 + 1)
+        self.set_label_font(8)
+        self.cell(
+            w=rect_width,
+            h=self.font_size,
+            align=Align.C,
+            txt=label,
+            new_x=XPos.LEFT,
+            new_y=YPos.NEXT,
+        )
+        if label_desc:
+            self.set_light_font(6)
+            self.set_xy(x=self.x, y=self.y + 1)
+            self.multi_cell(
+                w=rect_width,
+                h=self.font_size,
+                txt=label_desc,
+                align=Align.C,
+                new_x=XPos.LEFT,
+                new_y=YPos.TOP,
+            )
+        pass
+
+    def draw_group_profile_squares(self):
+        start_x = self.get_x()
+        start_y = self.get_y()
+        width = (self.w - self.l_margin - self.r_margin) / 2
+
+        self.draw_square(
+            start_x,
+            start_y,
+            width,
+            width,
+            (255, 240, 193),
+            "Интеграторы",
+            "отвечают за целостность команды и культуры в долгосрочной перспективе",
+        )
+        self.draw_square(
+            start_x + width,
+            start_y,
+            width,
+            width,
+            (253, 219, 246),
+            "Предприниматели",
+            "отвечают за стратегически важные и долгосрочные задачи",
+        )
+
+        self.draw_square(
+            start_x,
+            start_y + width,
+            width,
+            width,
+            (217, 245, 251),
+            "Администраторы",
+            "отвечают за эффективность, технологичность и низкие расходы здесь и сейчас",
+        )
+        # 4
+        self.draw_square(
+            start_x + width,
+            start_y + width,
+            width,
+            width,
+            (226, 239, 218),
+            "Производители",
+            "достигают краткосрочных результатов здесь и сейчас",
+        )
+
+        self.set_text_font(8)
+        self.text(start_x + width / 2 - 13, start_y - 2.5, "Фокус на процесс")
+        self.text(
+            start_x + width / 2 + width - 13.5, start_y - 2.5, "Фокус на результат"
+        )
+        with self.rotation(
+            90, self.l_margin - self.font_size, start_y + width / 2 + 10
+        ):
+            self.text(0, start_y + width / 2 + 10, "Неструктурированный подход")
+        with self.rotation(270, start_x + width * 2 + 3, start_y + width / 2 - 18):
+            self.text(
+                start_x + width * 2 + 3,
+                start_y + width / 2 - 18,
+                "Концептуальные решения",
+            )
+        with self.rotation(
+            90, self.l_margin - self.font_size, start_y + width / 2 + 7 + width
+        ):
+            self.text(
+                0,
+                start_y + width / 2 + 7 + width,
+                "Структурированный подход",
+            )
+        with self.rotation(
+            270,
             start_x + width * 2 + 3,
             start_y + width / 2 - 15 + width,
-            "Локальные решения",
+        ):
+            self.text(
+                start_x + width * 2 + 3,
+                start_y + width / 2 - 15 + width,
+                "Локальные решения",
+            )
+
+        self.set_light_font(8)
+
+        # self.text(start_x + width / 4 - 3, start_y + 4, 'ESFJ')
+        # self.text(start_x + width / 4 - 13, start_y + 7, 'Массовик-затейник')
+        self.text(start_x + width / 4 - 13, start_y + 4, "Массовик-затейник")
+
+        # self.text(start_x + width * (3/4) - 3, start_y + 4, 'ENFJ')
+        # self.text(start_x + width * (3/4) - 13, start_y + 7, 'Идеалист-харизматик')
+        self.text(start_x + width * (3 / 4) - 12, start_y + 4, "Чуткий наставник")
+
+        # self.text(start_x + width + width / 4 - 3, start_y + 4, 'ESTJ')
+        # self.text(start_x + width + width / 4 - 14, start_y + 7, 'Контролер по жизни')
+        self.text(start_x + width + width / 4 - 8, start_y + 4, "Контролер")
+
+        # self.text(start_x + width + width * (3/4) - 3, start_y + 4, 'ENTJ')
+        # self.text(start_x + width + width * (3/4) - 11, start_y + 7, 'Предприниматель')
+        self.text(start_x + width + width * (3 / 4) - 5, start_y + 4, "Аналитик")
+
+        # self.text(start_x + width / 4 - 3, start_y + width - 6 , 'ESFP')
+        # self.text(start_x + width / 4 - 18.5, start_y + width - 3 , 'Спонтанный коммуникатор')
+        self.text(
+            start_x + width / 4 - 9,
+            start_y + width - 3,
+            "Развлекатель",
         )
 
-    pdf.set_light_font(8)
+        # self.text(start_x + width * (3/4) - 3, start_y + width - 6 , 'ENFP')
+        # self.text(start_x + width * (3/4) - 7, start_y + width - 3 , 'Инициатор')
+        self.text(
+            start_x + width * (3 / 4) - 7,
+            start_y + width - 3,
+            "Мотиватор",
+        )
 
-    # pdf.text(start_x + width / 4 - 3, start_y + 4, 'ESFJ')
-    # pdf.text(start_x + width / 4 - 13, start_y + 7, 'Массовик-затейник')
-    pdf.text(start_x + width / 4 - 13, start_y + 4, "Массовик-затейник")
+        # self.text(start_x + width + width / 4 - 3, start_y + width - 6 , 'ESTP')
+        # self.text(start_x + width + width / 4 - 10, start_y + width - 3 , 'Ультра-реалист')
+        self.text(
+            start_x + width + width / 4 - 12,
+            start_y + width - 3,
+            "Искатель ресурсов",
+        )
 
-    # pdf.text(start_x + width * (3/4) - 3, start_y + 4, 'ENFJ')
-    # pdf.text(start_x + width * (3/4) - 13, start_y + 7, 'Идеалист-харизматик')
-    pdf.text(start_x + width * (3 / 4) - 12, start_y + 4, "Чуткий наставник")
+        # self.text(start_x + width + width * (3/4) - 3, start_y + width - 6 , 'ENTP')
+        self.text(
+            start_x + width + width * (3 / 4) - 9,
+            start_y + width - 3,
+            "Изобретатель",
+        )
 
-    # pdf.text(start_x + width + width / 4 - 3, start_y + 4, 'ESTJ')
-    # pdf.text(start_x + width + width / 4 - 14, start_y + 7, 'Контролер по жизни')
-    pdf.text(start_x + width + width / 4 - 8, start_y + 4, "Контролер")
+        # self.text(start_x + width / 4 - 3, start_y + width + 4 , 'ISFJ')
+        # self.text(start_x + width / 4 - 7, start_y + width + 7 , 'Хранитель')
+        self.text(start_x + width / 4 - 7, start_y + width + 4, "Хранитель")
 
-    # pdf.text(start_x + width + width * (3/4) - 3, start_y + 4, 'ENTJ')
-    # pdf.text(start_x + width + width * (3/4) - 11, start_y + 7, 'Предприниматель')
-    pdf.text(start_x + width + width * (3 / 4) - 5, start_y + 4, "Аналитик")
+        # self.text(start_x + width * (3/4) - 3, start_y + width + 4 , 'INFJ')
+        # self.text(start_x + width * (3/4) - 9, start_y + width + 7 , 'Вдохновитель')
+        self.text(
+            start_x + width * (3 / 4) - 9,
+            start_y + width + 4,
+            "Вдохновитель",
+        )
 
-    # pdf.text(start_x + width / 4 - 3, start_y + width - 6 , 'ESFP')
-    # pdf.text(start_x + width / 4 - 18.5, start_y + width - 3 , 'Спонтанный коммуникатор')
-    pdf.text(
-        start_x + width / 4 - 9,
-        start_y + width - 3,
-        "Развлекатель",
-    )
+        # self.text(start_x + width + width / 4 - 3, start_y + width + 4 , 'ISTJ')
+        # self.text(start_x + width + width / 4 - 8, start_y + width + 7 , 'Организатор')
+        self.text(
+            start_x + width + width / 4 - 6,
+            start_y + width + 4,
+            "Организатор",
+        )
 
-    # pdf.text(start_x + width * (3/4) - 3, start_y + width - 6 , 'ENFP')
-    # pdf.text(start_x + width * (3/4) - 7, start_y + width - 3 , 'Инициатор')
-    pdf.text(
-        start_x + width * (3 / 4) - 7,
-        start_y + width - 3,
-        "Мотиватор",
-    )
+        # self.text(start_x + width + width * (3/4) - 3, start_y + width + 4 , 'INTJ')
+        # self.text(start_x + width + width * (3/4) - 14, start_y + width + 7 , 'Любитель улучшений')
+        self.text(
+            start_x + width + width * (3 / 4) - 14,
+            start_y + width + 4,
+            "Любитель улучшений",
+        )
 
-    # pdf.text(start_x + width + width / 4 - 3, start_y + width - 6 , 'ESTP')
-    # pdf.text(start_x + width + width / 4 - 10, start_y + width - 3 , 'Ультра-реалист')
-    pdf.text(
-        start_x + width + width / 4 - 12,
-        start_y + width - 3,
-        "Искатель ресурсов",
-    )
+        # self.text(start_x + width / 4 - 3, start_y + width + width - 6 , 'ISFP')
+        # self.text(start_x + width / 4 - 7, start_y + width + width - 3 , 'Посредник')
+        self.text(
+            start_x + width / 4 - 5,
+            start_y + width + width - 3,
+            "Опекун",
+        )
 
-    # pdf.text(start_x + width + width * (3/4) - 3, start_y + width - 6 , 'ENTP')
-    pdf.text(
-        start_x + width + width * (3 / 4) - 9,
-        start_y + width - 3,
-        "Изобретатель",
-    )
+        # self.text(start_x + width * (3/4) - 3, start_y + width + width - 6 , 'INFP')
+        self.text(
+            start_x + width * (3 / 4) - 16,
+            start_y + width + width - 3,
+            "Благородный служитель",
+        )
 
-    # pdf.text(start_x + width / 4 - 3, start_y + width + 4 , 'ISFJ')
-    # pdf.text(start_x + width / 4 - 7, start_y + width + 7 , 'Хранитель')
-    pdf.text(start_x + width / 4 - 7, start_y + width + 4, "Хранитель")
+        # self.text(start_x + width + width / 4 - 3, start_y + width + width - 6 , 'ISTP')
+        # self.text(start_x + width + width / 4 - 12, start_y + width + width - 3 , 'Экспериментатор')
+        self.text(
+            start_x + width + width / 4 - 9,
+            start_y + width + width - 3,
+            "Исполнитель",
+        )
 
-    # pdf.text(start_x + width * (3/4) - 3, start_y + width + 4 , 'INFJ')
-    # pdf.text(start_x + width * (3/4) - 9, start_y + width + 7 , 'Вдохновитель')
-    pdf.text(
-        start_x + width * (3 / 4) - 9,
-        start_y + width + 4,
-        "Вдохновитель",
-    )
+        # self.text(start_x + width + width * (3/4) - 3, start_y + width + width - 6 , 'INTP')
+        self.text(
+            start_x + width + width * (3 / 4) - 12,
+            start_y + width + width - 3,
+            "Решатель проблем",
+        )
 
-    # pdf.text(start_x + width + width / 4 - 3, start_y + width + 4 , 'ISTJ')
-    # pdf.text(start_x + width + width / 4 - 8, start_y + width + 7 , 'Организатор')
-    pdf.text(
-        start_x + width + width / 4 - 6,
-        start_y + width + 4,
-        "Организатор",
-    )
+        self.set_line_width(0.5)
+        self.set_draw_color(240)
 
-    # pdf.text(start_x + width + width * (3/4) - 3, start_y + width + 4 , 'INTJ')
-    # pdf.text(start_x + width + width * (3/4) - 14, start_y + width + 7 , 'Любитель улучшений')
-    pdf.text(
-        start_x + width + width * (3 / 4) - 14,
-        start_y + width + 4,
-        "Любитель улучшений",
-    )
+        pass
 
-    # pdf.text(start_x + width / 4 - 3, start_y + width + width - 6 , 'ISFP')
-    # pdf.text(start_x + width / 4 - 7, start_y + width + width - 3 , 'Посредник')
-    pdf.text(
-        start_x + width / 4 - 5,
-        start_y + width + width - 3,
-        "Опекун",
-    )
-
-    # pdf.text(start_x + width * (3/4) - 3, start_y + width + width - 6 , 'INFP')
-    pdf.text(
-        start_x + width * (3 / 4) - 16,
-        start_y + width + width - 3,
-        "Благородный служитель",
-    )
-
-    # pdf.text(start_x + width + width / 4 - 3, start_y + width + width - 6 , 'ISTP')
-    # pdf.text(start_x + width + width / 4 - 12, start_y + width + width - 3 , 'Экспериментатор')
-    pdf.text(
-        start_x + width + width / 4 - 9,
-        start_y + width + width - 3,
-        "Исполнитель",
-    )
-
-    # pdf.text(start_x + width + width * (3/4) - 3, start_y + width + width - 6 , 'INTP')
-    pdf.text(
-        start_x + width + width * (3 / 4) - 12,
-        start_y + width + width - 3,
-        "Решатель проблем",
-    )
-
-    pdf.set_line_width(0.5)
-    pdf.set_draw_color(240)
-
-    pass
-
-
-def _draw_section_header(pdf: ZeticPDF, text: str):
-    pdf.set_fill_color(230, 230, 227)
-    pdf.set_draw_color(230, 230, 227)
-    rect_width = 210
-    rect_height = 8
-    pdf.rect(pdf.x, pdf.y, rect_width, rect_height, "FD")
-    pdf.set_text_color(118, 134, 146)
-    pdf.cell(h=rect_height, txt=text, new_y=YPos.NEXT, new_x=XPos.LEFT)
+    def draw_section_header(self, text: str):
+        self.set_fill_color(230, 230, 227)
+        self.set_draw_color(230, 230, 227)
+        rect_width = 210
+        rect_height = 8
+        self.rect(self.x, self.y, rect_width, rect_height, "FD")
+        self.set_text_color(118, 134, 146)
+        self.cell(h=rect_height, txt=text, new_y=YPos.NEXT, new_x=XPos.LEFT)
 
 
 class GroupReport:
-    _pdf: ZeticPDF
+    _pdf: ZeticGroupPDF
     data: GroupReportData
 
     def __init__(self):
-        self._pdf = ZeticPDF(orientation="P", unit="mm", format="A4")
+        self._pdf = ZeticGroupPDF(orientation="P", unit="mm", format="A4")
 
     def generate_pdf(self, data: GroupReportData) -> bytes:
         self.data = data
@@ -573,7 +605,7 @@ class GroupReport:
         self._add_page_header("Командный профиль: распределение внимания в группе")
 
         pdf.set_y(pdf.get_y() + 5)
-        _draw_group_profile_squares(pdf)
+        pdf.draw_group_profile_squares()
 
         pass
 
@@ -594,7 +626,7 @@ class GroupReport:
         sections = CATTELL_CATEGORIES
         for section in sections:
             section_label = TRANSLATIONS_DICT.get_translation(section, lang)
-            _draw_section_header(pdf, section_label)
+            pdf.draw_section_header(section_label)
             categories = CATTELL_CATEGORIES[section]
             pdf.set_y(pdf.get_y() + 2)
 
@@ -622,7 +654,7 @@ class GroupReport:
         scales = COPING_CATEGORIES_V1
         for scale in scales:
             scale_label = TRANSLATIONS_DICT.get_translation(scale, lang)
-            _draw_section_header(pdf, scale_label)
+            pdf.draw_section_header(scale_label)
 
             categories = COPING_CATEGORIES_V1[scale]
             pdf.set_y(pdf.get_y() + 2)
@@ -650,7 +682,7 @@ class GroupReport:
         scales = BOYKO_CATEGORIES_V1
         for scale in scales:
             scale_label = TRANSLATIONS_DICT.get_translation(scale, lang)
-            _draw_section_header(pdf, scale_label)
+            pdf.draw_section_header(scale_label)
 
             categories = BOYKO_CATEGORIES_V1[scale]
             pdf.set_y(pdf.get_y() + 2)
@@ -678,7 +710,7 @@ class GroupReport:
         scales = VALUES_CATEGORIES_V1
         for scale in scales:
             scale_label = TRANSLATIONS_DICT.get_translation(scale, lang)
-            _draw_section_header(pdf, scale_label)
+            pdf.draw_section_header(scale_label)
 
             categories = VALUES_CATEGORIES_V1[scale]
             pdf.set_y(pdf.get_y() + 2)
