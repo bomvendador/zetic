cdc2d172feb9e239cd4f9edec132958074b443e6
Алексей <bomvendador@yandex.ru>
2024-06-20 21:51:24 +0300

    new version

--- panel/views.py
+++ panel/views.py
@@ -384,7 +384,7 @@ def get_participants_data_for_group_report(participants_ids):
                 categories_fits_cnt = 0
                 for filter_category in filter_categories:
                     report_data_categories_points_sum = ReportData.objects.filter(Q(report=report) & Q(category_code=filter_category.category.code)).aggregate(Sum('points'))
-                    if filter_category.points_from <= report_data_categories_points_sum <= filter_category.points_to:
+                    if filter_category.points_from <= len(report_data_categories_points_sum) <= filter_category.points_to:
                         categories_fits_cnt = categories_fits_cnt + 1
 
                     # for data in report_data_inst:
