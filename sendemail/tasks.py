d9d00ca543fd1191b340af16bc065933a79d49b2
Алексей <bomvendador@yandex.ru>
2024-10-06 00:25:09 +0300

    изменена работа с celery

--- sendemail/tasks.py
+++ sendemail/tasks.py
@@ -27,101 +27,103 @@ from pdf.page6_file import page6
 from pdf.page_conclusions import page as page_short_conclusions
 from pdf import raw_to_t_point
 from pdf.save_data import save_data_to_db_and_send_report
-from pdf.views import save_serve_file, category_data
+from pdf.views import save_serve_file, category_data, pdf_single_generator
 
 import cyrtranslit
 
 
 @shared_task(name='pdf_single_generator')
 def pdf_single_generator_task(questionnaire_id, report_id):
-    time_start = time.perf_counter()
-    pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
-    pdf.add_font("Cambria", style="",
-                 fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Cambria.ttf", uni=True)
-    pdf.add_font("Cambria-Bold", style="",
-                 fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Cambria-Bold.ttf", uni=True)
-    pdf.add_font("RalewayMedium", style="",
-                 fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Medium.ttf", uni=True)
-    pdf.add_font("RalewayRegular", style="",
-                 fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Regular.ttf", uni=True)
-    pdf.add_font("RalewayLight", style="",
-                 fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Light.ttf", uni=True)
-    pdf.add_font("RalewayBold", style="", fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Bold.ttf",
-                 uni=True)
-    pdf.add_font("NotoSansDisplayMedium", style="",
-                 fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/NotoSansDisplay-Medium.ttf", uni=True)
-    pdf.add_page()
+    pdf_single_generator(questionnaire_id, report_id)
 
-    questionnaire_inst = Questionnaire.objects.get(id=questionnaire_id)
-    questionnaire_questions_answers_for_validity_inst = QuestionnaireQuestionAnswers.objects.filter(
-        questionnaire=questionnaire_inst,
-        question__category__for_validity=True)
-    employee = questionnaire_inst.participant.employee
-    if not report_id == '':
-        report_inst = Report.objects.get(id=report_id)
-        lie_points = report_inst.lie_points
-    else:
-        sum_lie_point = 0
-        for answer in questionnaire_questions_answers_for_validity_inst:
-            sum_lie_point = sum_lie_point + answer.answer.raw_point
-        lie_points = round(sum_lie_point / 40 * 10)
-    participant_info = {
-        "name": employee.name,
-        "sex": employee.sex.name_ru,
-        "birth_year": employee.birth_year,
-        "email": employee.email
-    }
-    # participant_name = participant_info['name']
-    lang = 'ru'
-    # lang = request_json['lang']
-    # lie_points = round(request_json['lie_points']/40 * 10)
-
-    title_page(pdf, employee.name, lang)
-
-    pdf.add_page()
-    page2(pdf, lie_points, lang)
-
-    pdf.add_page()
-    page_short_conclusions(pdf, questionnaire_id, 'ru', report_id)
-
-    answer_code_1 = category_data('1_', questionnaire_id, employee.id)
-    if answer_code_1:
-        pdf.add_page()
-        # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
-        page3(pdf, answer_code_1, lang, participant_info)
-
-    answer_code_2 = category_data('2_', questionnaire_id, employee.id)
-    if answer_code_2:
-        pdf.add_page()
-        # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
-        page4(pdf, answer_code_2, lang, participant_info)
-
-    answer_code_3 = category_data('3_', questionnaire_id, employee.id)
-    if answer_code_3:
-        pdf.add_page()
-        # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
-        page5(pdf, answer_code_3, lang, participant_info)
-
-    answer_code_4 = category_data('4_', questionnaire_id, employee.id)
-    if answer_code_4:
-        pdf.add_page()
-        # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
-        page6(pdf, answer_code_4, lang, participant_info)
-    now = datetime.now()
-
-    file_name = cyrtranslit.to_latin(questionnaire_inst.participant.employee.name.strip(), 'ru') + "_" + now.strftime(
-        "%d_%m_%Y__%H_%M_%S") + "_" + lang.upper() + '_single.pdf'
-
-    path = "media/reportsPDF/single/"
-
-    # save_data_to_db(request_json, file_name)
-    response = save_serve_file(pdf, path, file_name)
-
-    send_email_result = save_data_to_db_and_send_report(questionnaire_inst.id, file_name, questionnaire_inst.participant.study.id, lie_points, lang, report_id)
-
-    time_finish = time.perf_counter()
-    # print(round(time_finish-time_start, 2))
-    return response
+    # time_start = time.perf_counter()
+    # pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
+    # pdf.add_font("Cambria", style="",
+    #              fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Cambria.ttf", uni=True)
+    # pdf.add_font("Cambria-Bold", style="",
+    #              fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Cambria-Bold.ttf", uni=True)
+    # pdf.add_font("RalewayMedium", style="",
+    #              fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Medium.ttf", uni=True)
+    # pdf.add_font("RalewayRegular", style="",
+    #              fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Regular.ttf", uni=True)
+    # pdf.add_font("RalewayLight", style="",
+    #              fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Light.ttf", uni=True)
+    # pdf.add_font("RalewayBold", style="", fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/Raleway-Bold.ttf",
+    #              uni=True)
+    # pdf.add_font("NotoSansDisplayMedium", style="",
+    #              fname=os.path.join(settings.BASE_DIR, 'static/') + "/fonts/NotoSansDisplay-Medium.ttf", uni=True)
+    # pdf.add_page()
+    #
+    # questionnaire_inst = Questionnaire.objects.get(id=questionnaire_id)
+    # questionnaire_questions_answers_for_validity_inst = QuestionnaireQuestionAnswers.objects.filter(
+    #     questionnaire=questionnaire_inst,
+    #     question__category__for_validity=True)
+    # employee = questionnaire_inst.participant.employee
+    # if not report_id == '':
+    #     report_inst = Report.objects.get(id=report_id)
+    #     lie_points = report_inst.lie_points
+    # else:
+    #     sum_lie_point = 0
+    #     for answer in questionnaire_questions_answers_for_validity_inst:
+    #         sum_lie_point = sum_lie_point + answer.answer.raw_point
+    #     lie_points = round(sum_lie_point / 40 * 10)
+    # participant_info = {
+    #     "name": employee.name,
+    #     "sex": employee.sex.name_ru,
+    #     "birth_year": employee.birth_year,
+    #     "email": employee.email
+    # }
+    # # participant_name = participant_info['name']
+    # lang = 'ru'
+    # # lang = request_json['lang']
+    # # lie_points = round(request_json['lie_points']/40 * 10)
+    #
+    # title_page(pdf, employee.name, lang)
+    #
+    # pdf.add_page()
+    # page2(pdf, lie_points, lang)
+    #
+    # pdf.add_page()
+    # page_short_conclusions(pdf, questionnaire_id, 'ru', report_id)
+    #
+    # answer_code_1 = category_data('1_', questionnaire_id, employee.id)
+    # if answer_code_1:
+    #     pdf.add_page()
+    #     # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
+    #     page3(pdf, answer_code_1, lang, participant_info)
+    #
+    # answer_code_2 = category_data('2_', questionnaire_id, employee.id)
+    # if answer_code_2:
+    #     pdf.add_page()
+    #     # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
+    #     page4(pdf, answer_code_2, lang, participant_info)
+    #
+    # answer_code_3 = category_data('3_', questionnaire_id, employee.id)
+    # if answer_code_3:
+    #     pdf.add_page()
+    #     # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
+    #     page5(pdf, answer_code_3, lang, participant_info)
+    #
+    # answer_code_4 = category_data('4_', questionnaire_id, employee.id)
+    # if answer_code_4:
+    #     pdf.add_page()
+    #     # page3(pdf, extract_section(request_json, 'Кеттелл'), lang)
+    #     page6(pdf, answer_code_4, lang, participant_info)
+    # now = datetime.now()
+    #
+    # file_name = cyrtranslit.to_latin(questionnaire_inst.participant.employee.name.strip(), 'ru') + "_" + now.strftime(
+    #     "%d_%m_%Y__%H_%M_%S") + "_" + lang.upper() + '_single.pdf'
+    #
+    # path = "media/reportsPDF/single/"
+    #
+    # # save_data_to_db(request_json, file_name)
+    # response = save_serve_file(pdf, path, file_name)
+    #
+    # send_email_result = save_data_to_db_and_send_report(questionnaire_inst.id, file_name, questionnaire_inst.participant.study.id, lie_points, lang, report_id)
+    #
+    # time_finish = time.perf_counter()
+    # # print(round(time_finish-time_start, 2))
+    # return response
 
 
 def diff_month(start_date, end_date):
