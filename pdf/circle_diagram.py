cc817652336a70df41fce09b6bf98a9a36314609
Алексей <bomvendador@yandex.ru>
2024-11-03 11:25:27 +0300

    webdriver

--- pdf/circle_diagram.py
+++ pdf/circle_diagram.py
@@ -48,7 +48,7 @@ if DEBUG == 0:
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    web_driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+    web_driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=chrome_options)
 
 
 def page_circle_diagram(pdf, questionnaire_id, report_id, lang):
