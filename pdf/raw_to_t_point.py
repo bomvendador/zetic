0d41bd84d48192001a3675ef9ea943ae84177514
Алексей <bomvendador@yandex.ru>
2024-11-10 20:06:21 +0300

    filter_raw_points_to_t_points fix2

--- pdf/raw_to_t_point.py
+++ pdf/raw_to_t_point.py
@@ -30,7 +30,7 @@ def filter_raw_points_to_t_points(raw_point, employee_id, category_id):
         t_point_inst = RawToTPoints.objects.get(type=raw_points_filter, category=category, raw_points=raw_point)
     except RawToTPoints.DoesNotExist:
         raw_points_filter = RawToTPointsType.objects.get(is_default=True, age_gender_group=age_gender_group)
-        if RawToTPoints.objects.get(type=raw_points_filter, category=category, raw_points=raw_point).exists():
+        if RawToTPoints.objects.filter(type=raw_points_filter, category=category, raw_points=raw_point).exists():
             t_point_inst = RawToTPoints.objects.get(type=raw_points_filter, category=category, raw_points=raw_point)
         else:
             max_raw_point = RawToTPoints.objects.filter(type=raw_points_filter, category=category).aggregate(Max('raw_points'))
