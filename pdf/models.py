f4181d135e59244ee6ab249e618113ffde142827
Алексей <bomvendador@yandex.ru>
2024-11-18 22:23:51 +0300

    PositionField commented

--- pdf/models.py
+++ pdf/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 from django.utils import timezone
-from positions import PositionField
+# from positions import PositionField
 
 
 class ResearchTemplate(models.Model):
